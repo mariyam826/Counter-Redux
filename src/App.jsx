9075779105583d7c894062edc29ccdc073d5abda
mariyam <mariyammrym826@gmail.com>
2024-01-06 00:28:15 +0530

    counter using redux

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from "./component/Counter"
+
+
+function App() {
+
+  return (
+    <div style={{height:'100vh'}} className="w-100 d-flex justify-content-center align-items-center ">
+     <Counter />
+    </div>
+  )
+}
+
+export default App
